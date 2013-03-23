a44e0bbaa291950b98fe135b21e57c0827c960d7
Cel Tico <cel@cenics.net>
2013-03-23 04:46:15 +0100

    Update

--- src/QuElFinder/view/Helper/QuElFinder.php
+++ src/QuElFinder/view/Helper/QuElFinder.php
@@ -5,12 +5,12 @@
  * @Company: Cencis s.c.p.
  */
 
-namespace QuCKEditor\View\Helper;
+namespace QuElFinder\View\Helper;
 
 use Zend\View\Helper\AbstractHelper;
 use Zend\Json\Json;
 
-class QuCKEditor extends AbstractHelper
+class QuElFinder extends AbstractHelper
 {
 
     /**
@@ -34,17 +34,84 @@ class QuCKEditor extends AbstractHelper
      */
     public function __invoke($name,$options = array())
     {
+
+        $this->loaderCss( $this->Config['LoadCss'] );
+        $this->loaderJs(  $this->Config['LoadJs']  );
+
+        $this->view->headLink()->prependStylesheet(
+            $this->view->basePath($this->Config['BasePath'])
+                . '/jquery/ui-themes/smoothness/jquery-ui-1.10.1.custom.css'
+        );
+
+        $this->view->inlineScript()->prependFile(
+            $this->view->basePath($this->Config['BasePath'])
+                . '/jquery/jquery-ui-1.10.1.custom.min.js', 'text/javascript'
+        );
+
         $options = Json::encode(array_merge(
-            $this->Config['CKEDITOR'],
+            $this->Config['elFinder'],
             $options
-        ), true);
+        ),false,array('enableJsonExprFinder' => true));
 
         ?>
-        <script type="text/javascript" src="<?php echo $this->view->basePath($this->Config['BasePath']) . '/ckeditor.js' ?>"></script>
-        <script type="text/javascript" >
-            CKEDITOR.replace( '<?php echo $name; ?>',<?=$options?>);
+        <script type="text/javascript">
+            $().ready(function(){
+              $('#<?php echo $name; ?>').elfinder(<?=$options?>).elfinder('instance');
+            });
         </script>
-    <?
+        <?php
+    }
 
+
+    /**
+     * @param array $array
+     */
+    public function loaderJs($array = array())
+    {
+        foreach( array_reverse(  $this->Config['LoadJs']  ) as $dir => $asset){
+
+            if(is_int($dir)){
+
+                $this->view->inlineScript()->prependFile(
+                    $this->view->basePath($this->Config['BasePath'])
+                        . '/js/' . $asset .  '.js', 'text/javascript'
+                );
+
+            } else {
+
+                foreach($asset as $dirAsset){
+                    $this->view->inlineScript()->prependFile(
+                        $this->view->basePath($this->Config['BasePath'])
+                            . '/js/' . $dir .  '/' .  $dirAsset  .   '.js', 'text/javascript'
+                    );
+                }
+            }
+        }
+    }
+
+    /**
+     * @param array $array
+     */
+    public function loaderCss($array = array())
+    {
+        foreach( array_reverse(  $this->Config['LoadCss']  ) as $dir => $asset){
+
+            if(is_int($dir)){
+
+                $this->view->headLink()->prependStylesheet(
+                    $this->view->basePath($this->Config['BasePath'])
+                        . '/css/' . $asset .  '.css'
+                );
+
+            } else {
+
+                foreach($asset as $dirAsset){
+                    $this->view->headLink()->prependStylesheet(
+                        $this->view->basePath($this->Config['BasePath'])
+                            .  '/' . $dir .  '/' .   $dirAsset .   '.css'
+                    );
+                }
+            }
+        }
     }
 }
